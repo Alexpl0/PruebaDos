1ce0d86fc8a2686a002c15c4296df5733e9f2ee0
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-04-10 08:09:23 -0600

    Prueba

---
+++ get_Productos.php
@@ -0,0 +1,49 @@
+<?php
+include("dao_Guardar.php");
+$con = connection();
+
+$sql = "SELECT * FROM users";
+$query = mysqli_query($con, $sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="CSS/style.css" rel="stylesheet">
+    <title>Users CRUD</title>
+</head>
+
+<body>
+    <div class="users-table">
+        <h2>Productos registrados</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Marca</th>
+                    <th>Descripcion</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while ($row = mysqli_fetch_array($query)): ?>
+                    <tr>
+                        <th><?= $row['id'] ?></th>
+                        <th><?= $row['nombre'] ?></th>
+                        <th><?= $row['marca'] ?></th>
+                        <th><?= $row['descripcion'] ?></th>
+                       </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
