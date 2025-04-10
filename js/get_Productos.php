b9540d7423fbd4126e01d3a6acce9b86aa7d367d
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-04-10 08:43:04 -0600

    Execute

--- js/get_Productos.php
+++ js/get_Productos.php
@@ -4,9 +4,14 @@ $con = connection();
 
 
 try{
-    // Consulta para obtener los datos de los productos
-    $sql = "SELECT * FROM users";
-    $query = mysqli_query($con, $sql);
+    // Se crea una instancia de la clase LocalConector para manejar la conexión a la base de datos
+    $con = new LocalConector();
+    // Se llama al método conectar() para establecer la conexión a la base de datos
+    $conex=$con ->conectar();
+
+    $stmt = $conex->prepare("SELECT * FROM `Productos`");
+    // Se ejecuta la consulta
+    $stmt->execute();
 
 }catch (Exception $e){
     http_response_code(500);
