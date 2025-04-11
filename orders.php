52c77f3096df8316fccf1722470cc2f39e4038cb
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-04-11 11:56:15 -0600

    d

--- orders.php
+++ orders.php
@@ -52,7 +52,7 @@
         <div class="card text-white bg-success mb-3" style="width: 18rem;">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <h6 class="card-subtitle mb-2">Card subtitle</h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>
