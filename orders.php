7189d484750a897fc3cb8c13e00fd8a4a7dbebfe
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-04-11 11:50:24 -0600

    de

--- orders.php
+++ orders.php
@@ -1,5 +1,3 @@
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -49,14 +47,15 @@
 
     <div id="mainOrders">
         <h1 id="title1">Historial de Ordenes Generadas</h1>
-            <div class="card text-white bg-info mb-3" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
+        <h1 id="title2"></h1>
+        <div class="card text-white bg-info mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+                <h5 class="card-title">Info card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
+        </div>
 
     </div>
+
+</body>
