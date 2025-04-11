8d02e0fd60bdb11bc16aadcf768720696e13521c
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-04-11 11:46:00 -0600

    ordenes

---
+++ ordenesGeneradas.php
@@ -0,0 +1,62 @@
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperación de Contraseña</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    
+    <!-- Agregar Ionicons en el head con la versión más reciente -->
+    <script type="module" src="https://unpkg.com/ionicons@7.2.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.2.2/dist/ionicons/ionicons.js"></script>
+    
+    <!-- Archivos CSS locales -->
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/header.css">
+
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="header__logo">GRAMMER</a>
+        <ion-icon name="menu-outline" class="header__toggle" id="nav-toggle"></ion-icon>
+        <nav class="nav" id="nav-menu">
+            <div class="nav__content bd-grid">
+                <ion-icon name="close-outline" class="nav__close" id="nav-close"></ion-icon>
+                <div class="nav__perfil">
+                    <div class="nav__img">
+                        <img src="assets/logo/logo.png" alt="logoGRAMMER">
+                    </div>
+                    <div>
+                        <a href="#" class="nav__name">SPECIAL FREIGHT</a>
+                    </div>
+                </div> 
+                <div class="nav__menu">
+                    <ul class="nav__list">
+                        <li class="nav__item"><a href="index.php" class="nav__link">Home</a></li>
+                        <li class="nav__item"><a href="scanQr.php" class="nav__link">Nueva Orden</a></li>
+                        <li class="nav__item"><a href="salas.php" class="nav__link">Ordenes Generadas</a></li>
+                        <li class="nav__item"><a href="newQR.php" class="nav__link">Agregar Usuario</a></li>
+                        <li class="nav__item"><a href="ExcelToJson.php" class="nav__link">Graficas</a></li>
+                        <li class="nav__item"><a href="#" class="nav__link">Manual</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <div id="mainOrders">
+        <h1 id="title1">Historial de Ordenes Generadas</h1>
+            <div class="card text-white bg-primary mb-3" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>
+
+    </div>
