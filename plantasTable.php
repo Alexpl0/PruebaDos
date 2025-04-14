7740bd660eeb230a7b7714bb43683667bbaca992
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-04-14 08:42:01 -0600

    select2

--- plantasTable.php
+++ plantasTable.php
@@ -8,6 +8,8 @@ require_once __DIR__ . '/dao/elements/daoPlantas.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Plantas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Select2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 </head>
 <body>
     <div class="container mt-5">
@@ -30,5 +32,19 @@ require_once __DIR__ . '/dao/elements/daoPlantas.php';
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
     </div>
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Select2 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script>
+        // Inicializar Select2 en el elemento <select>
+        $(document).ready(function() {
+            $('#planta').select2({
+                placeholder: "Busca una planta",
+                allowClear: true
+            });
+        });
+    </script>
 </body>
 </html>
