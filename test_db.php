890f67598b3310b847b4c36c1715f80f4b5912fc
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-04-15 12:17:00 -0600

    conex

--- test_db.php
+++ test_db.php
@@ -8,32 +8,20 @@ try {
     // Se llama al método conectar() para establecer la conexión a la base de datos
     $conex = $con->conectar();
 
-    // Verificar si se proporciona un usuario como parámetro
-    if (isset($_GET['usuario']) && !empty($_GET['usuario'])) {
-        $usuario = $_GET['usuario'];
+    // Recuperar datos de la base de datos
+    $stmt = $conex->prepare("SELECT * FROM `Usuarios` WHERE `Username` = 'Alex'");
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-        // Recuperar datos de la base de datos filtrando por usuario
-        $stmt = $conex->prepare("SELECT * FROM `CUENTAS` WHERE `USUARIO` LIKE ?");
-        $likeUsuario = "%$usuario%";
-        $stmt->bind_param("s", $likeUsuario);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        $datos = [];
-        while ($row = $result->fetch_assoc()) {
-            $datos[] = $row;
-        }
-
-        // Enviar datos como JSON
-        echo json_encode(['status' => 'success', 'data' => $datos]);
-
-        $stmt->close(); // Cierra la declaración preparada.
-    } else {
-        // Si no se proporciona un usuario, devolver un error
-        http_response_code(400);
-        echo json_encode(["success" => false, "mensaje" => "El parámetro 'usuario' es requerido."]);
+    $datos = [];
+    while ($row = $result->fetch_assoc()) {
+        $datos[] = $row;
     }
 
+    // Enviar datos como JSON
+    echo json_encode(['status' => 'success', 'data' => $datos]);
+
+    $stmt->close(); // Cierra la declaración preparada.
     $conex->close(); // Cierra la conexión a la base de datos.
 
 } catch (Exception $e) {
