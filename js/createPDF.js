725390099ec878cfc8e7f38e407c2c37c9156a95
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-04-21 11:45:05 -0600

    createJS

---
+++ js/createPDF.js
@@ -0,0 +1,22 @@
+function loadImage(url) {
+    return new Promise((resolve) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'blob';
+        xhr.onload = function(){
+            const reader = new FileReader();
+            reader.onload = function(event){
+                const res = event.target.result;
+                resolve(res);  
+            };
+            const file = this.response;
+            reader.readAsDataURL(file);
+        }
+        xhr.send();
+    });
+}
+
+window.addEventListener('load', async () => {
+    const image = await loadImage('../assets/media/SPECIAL_FREIGHT_AUTHORIZATION.png');
+    console.log(image);
+});
