2ac267c48535c375359387f63324c7412830ba03
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-06 13:02:43 -0600

    endpoint

--- dao/elements/daoLocation.php
+++ dao/elements/daoLocation.php
@@ -2,12 +2,17 @@
 
 include_once('../db/db.php');
 
+// Fuerza la codificación UTF-8 para evitar conflictos con acentos
+header('Content-Type: application/json; charset=utf-8');
 
 try{
     // Se crea una instancia de la clase LocalConector para manejar la conexión a la base de datos
     $con = new LocalConector();
     // Se llama al método conectar() para establecer la conexión a la base de datos
-    $conex=$con ->conectar();
+    $conex = $con->conectar();
+
+    // Establece la codificación de la conexión a UTF-8
+    $conex->set_charset("utf8mb4");
 
     // Recuperar datos de la base de datos
     $stmt = $conex->prepare("SELECT * FROM `Location`");
@@ -19,14 +24,13 @@ try{
         $datos[] = $row;
     }
 
-// Enviar datos como JSON
-    echo json_encode(['status' => 'success', 'data' => $datos]);
+    // Enviar datos como JSON embellecido y sin conflictos de acentos
+    echo json_encode(['status' => 'success', 'data' => $datos], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
     $stmt->close(); // Cierra la declaración preparada.
     $conex->close(); // Cierra la conexión a la base de datos.
 
-
 } catch (Exception $e){
     http_response_code(500);
-    echo json_encode(["success" => false, "mensaje" => $e->getMessage()]);
+    echo json_encode(["success" => false, "mensaje" => $e->getMessage()], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 }
