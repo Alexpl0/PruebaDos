4b539b83ac58eb4e1163e5a04527d0c401488c68
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-08 13:25:45 -0600

    ko

--- newOrder.php
+++ newOrder.php
@@ -16,6 +16,11 @@ require_once __DIR__ . '/dao/elements/daoStates.php';
 session_start();
 include_once 'dao/users/auth_check.php';
 ?>
+<script>
+    window.authorizationLevel = <?php echo json_encode(isset($_SESSION['user']['authorization_level']) ? $_SESSION['user']['authorization_level'] : null); ?>;
+    window.userName = <?php echo json_encode(isset($_SESSION['user']['name']) ? $_SESSION['user']['name'] : null); ?>;
+    window.userID = <?php echo json_encode(isset($_SESSION['user']['id']) ? $_SESSION['user']['id'] : null); ?>;
+</script>
 
 <!DOCTYPE html>
 <html lang="en">
