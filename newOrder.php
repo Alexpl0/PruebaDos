3fa88409a2406f20692e8b0dc84aa1f7ad11a2d9
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-08 13:13:12 -0600

    stules

--- newOrder.php
+++ newOrder.php
@@ -38,7 +38,6 @@ include_once 'dao/users/auth_check.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
     <!-- Local CSS files -->
-    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/newOrder.css">
 
