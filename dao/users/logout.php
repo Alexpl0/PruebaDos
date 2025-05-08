333e3a72e9d4ffd78c61bb74c7859c43e5de8d32
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-08 10:05:04 -0600

    ...

--- dao/users/logout.php
+++ dao/users/logout.php
@@ -4,6 +4,6 @@ session_unset(); // Elimina todas las variables de sesión
 session_destroy(); // Destruye la sesión
 
 // Redirige al usuario al inicio de sesión o a la página principal
-header("Location: index.php");
+header("Location: ../../index.php");
 exit();
 ?>
