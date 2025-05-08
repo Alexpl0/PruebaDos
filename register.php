a166d1944b1f65c59c2053f24442634143ede26e
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-08 11:31:48 -0600

    name

--- register.php
+++ register.php
@@ -65,8 +65,8 @@ session_start();
                             <input type="email" class="form-control" id="email" name="email" required>
                         </div>
                         <div class="mb-3">
-                            <label for="username" class="form-label">Username</label>
-                            <input type="text" class="form-control" id="username" name="username" required>
+                            <label for="name" class="form-label">Complete Name</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
