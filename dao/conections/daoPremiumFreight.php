c4f63078074a4cca0966a8c33bfe9b30c70249e9
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-14 10:38:53 -0600

    dao

--- dao/conections/daoPremiumFreight.php
+++ dao/conections/daoPremiumFreight.php
@@ -8,7 +8,7 @@ try {
 
     // Consulta SQL con LEFT JOIN y alias para evitar conflictos de nombres
     $sql = "
-        SELECT 
+            SELECT 
             pf.*,
             u.name AS creator_name,
             u.email AS creator_email,
@@ -21,22 +21,25 @@ try {
             lo_to.city AS destiny_city,
             lo_to.state AS destiny_state,
             lo_to.zip AS destiny_zip,
+            c.name AS carrier,
             st.id AS statusid,
             st.name AS status_name,
             pfa.id AS approval_id,
             pfa.approval_date,
-            pfa.act_approv as approval_status,
+            pfa.act_approv AS approval_status,
             u_approver.name AS approver_name,
             u_approver.email AS approver_email,
             u_approver.role AS approver_role
         FROM PremiumFreight pf
+        LEFT JOIN Carriers c ON pf.carrier_id = c.id
         LEFT JOIN User u ON pf.user_id = u.id
         LEFT JOIN Location lo_from ON pf.origin_id = lo_from.id
         LEFT JOIN Location lo_to ON pf.destiny_id = lo_to.id
         LEFT JOIN Status st ON pf.status_id = st.id
         LEFT JOIN PremiumFreightApprovals pfa ON pf.id = pfa.premium_freight_id
         LEFT JOIN User u_approver ON pfa.user_id = u_approver.id
-        ORDER BY pf.id DESC
+        ORDER BY pf.id DESC;
+
     ";
 
 
