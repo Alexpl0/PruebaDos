b33c62cbf85354f1ba2fa2d60267737a9c53c380
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-16 15:08:12 -0600

    ko

--- profile.php
+++ profile.php
@@ -25,7 +25,7 @@ include_once 'dao/users/auth_check.php';
     <!-- Google Material Symbols (iconos) -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     
-    
+
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
