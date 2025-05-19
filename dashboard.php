0fcbfdc319f4a4dbbe1076d2fdf89ae191466f73
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-19 13:01:47 -0600

    ko

--- dashboard.php
+++ dashboard.php
@@ -22,8 +22,6 @@ include_once 'dao/users/auth_check.php';
     <title>Premium Freight Dashboard</title>
     
     <!-- ================== CSS DE TERCEROS ================== -->
-    <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     
     <!-- Chart.js -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.css">
@@ -43,6 +41,9 @@ include_once 'dao/users/auth_check.php';
     <!-- SweetAlert2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
 
+    <!-- Bootstrap 5 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
     <!-- DataTables -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 
