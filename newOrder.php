7a409479af5d966a5bf6759f17fe70457ef7de84
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-05-19 11:15:01 -0600

    ok

--- newOrder.php
+++ newOrder.php
@@ -54,18 +54,6 @@ include_once 'dao/users/auth_check.php';
     <!-- Google Fonts -->
     <link rel="preload" href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&display=swap" as="style">
 
-    <style>
-    body {
-      font-family: Arial, sans-serif;
-      padding: 20px;
-    }
-
-    .icono {
-      font-size: 24px;
-      margin-right: 10px;
-      color: #333;
-    }
-  </style>
 
 </head>
 <body>
