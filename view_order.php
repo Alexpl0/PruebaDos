694aa4b73a5c2cfd5df872f34282920ab55a3029
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-06-09 11:19:44 -0600

    css

--- view_order.php
+++ view_order.php
@@ -114,6 +114,7 @@ $URLM = "https://grammermx.com/Mailer/PFMailer/";
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/view-order.css">
     
     <!-- External JS -->
