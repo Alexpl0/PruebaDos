9c65c3e1de6c6e11cc1d4254aac128d7e010adc7
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-06-11 13:18:06 -0600

    icon

--- orders.php
+++ orders.php
@@ -36,6 +36,7 @@ $plant = isset($_SESSION['user']['plant']) ? $_SESSION['user']['plant'] : null;
 <head>
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
     <title>Orders</title>
     
     <!-- SweetAlert2 -->
