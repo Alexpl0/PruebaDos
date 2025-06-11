da907b15923271f734d43ebc20e84bb2ce2685f4
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-06-11 10:37:43 -0600

    DataTables

--- newOrder.php
+++ newOrder.php
@@ -28,7 +28,7 @@ include_once 'dao/users/auth_check.php';
     window.userID = <?php echo json_encode($userID); ?>;
     window.userPlant = <?php echo json_encode($plant); ?>;
     // Definimos la variable global de JavaScript con la URL base desde PHP
-    const URL = '<?php echo URL; ?>'; 
+    const URLPF = '<?php echo URLPF ?>'; 
 </script>
 
 <!DOCTYPE html>
