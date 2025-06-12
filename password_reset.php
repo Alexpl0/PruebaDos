c244c8364bb80e59b205d836364db9fdd5a7c124
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-06-12 09:25:27 -0600

    try

--- password_reset.php
+++ password_reset.php
@@ -144,7 +144,7 @@ if (!$tokenValid) {
                     </div>
                 </div>
                 
-                <div class="form-group">
+                <div class="form-group" style="display: flex; justify-content: center;">
                     <button type="submit" id="reset-btn" class="btn btn-reset">
                         <i class="fas fa-key"></i> Reset Password
                     </button>
