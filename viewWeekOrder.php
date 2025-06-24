a473c8377e755dd23fa349d1da1c7c670b0bd886
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-06-24 15:09:07 -0600

    ok viewWeekorder

--- viewWeekOrder.php
+++ viewWeekOrder.php
@@ -6,6 +6,12 @@
 
 require_once 'dao/users/auth_check.php';
 
+// Check if user is authenticated
+if (!isset($_SESSION['user'])) {
+    http_response_code(401);
+    die('User not authenticated');
+}
+
 // Get user session data from auth_check
 $userId = $user_id;
 $userName = $user_name;
@@ -154,5 +160,29 @@ $URLPF = "https://grammermx.com/PremiumFreight/";
             </div>
         </div>
     </div>
+
+    <!-- PDF and Canvas Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+    <script src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
+    
+    <!-- jQuery and Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- DataTables JS -->
+    <script type="text/javascript" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+
+    <!-- Custom scripts -->
+    <script src="js/uploadFiles.js"></script>
+    <script type="module" src="js/viewWeekorder.js"></script>
+
+    <script>console.log('User Plant:', window.userPlant);</script>
 </body>
 </html>
