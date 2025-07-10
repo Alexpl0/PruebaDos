5ec9fe99b9ff5791b8067cf3d9d12a10e549c0a6
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-07-10 10:08:20 -0600

    php

--- adminUsers.php
+++ adminUsers.php
@@ -136,7 +136,6 @@ require_once 'dao/users/context_injector.php';
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/header.js"></script>
-    <script src="js/PasswordManager.js"></script>
     <script src="js/userAdmin.js"></script>
     
     <?php if (isset($appContextForJS['user']['authorizationLevel']) && $appContextForJS['user']['authorizationLevel'] > 0): ?>
