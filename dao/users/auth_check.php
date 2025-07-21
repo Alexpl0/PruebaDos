414e8a5d4d30d115e2573099fa83357d8fca05df
Ex-Perez-J <extern.jesus.perez@grammer.com>
2025-07-21 10:18:41 -0600

    cors2

--- dao/users/auth_check.php
+++ dao/users/auth_check.php
@@ -1,4 +1,13 @@
 <?php
+// --- CORS GLOBAL ---
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 session_start();
 
 // Lista de páginas públicas (solo para usuarios NO logueados)
