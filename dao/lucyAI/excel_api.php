543408ae148960961fc28ffad10bd32f60e0b731
Alejandro Pérez <j.alejandro.pl@gmail.com>
2025-10-05 20:04:23 -0600

    ei

--- dao/lucyAI/excel_api.php
+++ dao/lucyAI/excel_api.php
@@ -32,7 +32,7 @@ define('MICROSOFT_GRAPH_URL', 'https://graph.microsoft.com/v1.0');
 
 // Usuario donde se guardarán los archivos Excel
 // Puede ser un email (user@tudominio.com) o un User ID
-define('ONEDRIVE_USER', 'j.alejandro.pl_gmail.com#EXT#@jalejandroplgmail.onmicrosoft.com');
+define('ONEDRIVE_USER', 'JesusPerez@Alexdev043.onmicrosoft.com');
 
 
 // ==================== OBTENER DATOS DEL REQUEST ====================
