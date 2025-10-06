85d12e51b4915923790388762cd5bfba292a7625
Alejandro Pérez <j.alejandro.pl@gmail.com>
2025-10-05 22:06:32 -0600

    New Workspace?

--- dao/lucyAI/powerbi_api.php
+++ dao/lucyAI/powerbi_api.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['user'])) {
 define('POWERBI_CLIENT_ID', '07297fbf-6ba4-4fe3-99a1-87c93a5aaeb4');
 define('POWERBI_CLIENT_SECRET', 'PDK8Q~F-ZLtKAYWVaOHVMDq63rccxXSZ2M4-pae3');
 define('POWERBI_TENANT_ID', '1b76d39b-fc45-4afe-a05b-8d8f81f18a77');
-define('POWERBI_WORKSPACE_ID', '40fcad50-6205-47bd-adcb-c2cacdbff3a9'); // ID del workspace "Pruebas Grammer"
+define('POWERBI_WORKSPACE_ID', '959d9628-e3b1-4217-aaed-ea2c0d90dc8a'); // ID del workspace "Pruebas Grammer"
 
 
 define('POWERBI_API_URL', 'https://api.powerbi.com/v1.0/myorg');
