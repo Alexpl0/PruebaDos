c07800435f540712604d1ff900b87626f5cf16d0
Alejandro <j.alejandro.pl@gmail.com>
2025-10-14 09:04:03 -0600

    Get

--- cotizaciones/dao/daoGetRequests.php
+++ cotizaciones/dao/daoGetRequests.php
@@ -10,25 +10,6 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/db/db.php'; 
 
-// Helper function to set CORS headers
-function setCorsHeaders() {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type');
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        http_response_code(200);
-        exit();
-    }
-}
-
-// Helper function for JSON responses
-function sendJsonResponse($success, $message, $data = null, $statusCode = 200) {
-    http_response_code($statusCode);
-    header('Content-Type: application/json');
-    echo json_encode(['success' => $success, 'message' => $message, 'data' => $data]);
-    exit();
-}
-
 setCorsHeaders();
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
